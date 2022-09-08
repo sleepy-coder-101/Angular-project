139857712a46138c40977ef44a6431544a00fb2e
Monirul <monirul.work.1729@gmail.com>
2022-09-08 15:36:55 +0530

    Made a forms input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,22 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'my-first-app';
+  count = 0;
+
+  listOfNumbers = [1, 32, 32, 55];
+  listOfCars = ['Audi', 'BMW', 'Chevrolet', 'Dodge', 'Ford', 'GMC'];
+
+  decreaseCount = () => {
+    console.log('Button clicked');
+    this.count--;
+  };
+
+  increaseCount = () => {
+    console.log('Button clicked');
+    this.count++;
+  };
 }
